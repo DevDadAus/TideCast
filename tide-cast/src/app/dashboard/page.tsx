8041263509901bf9969843e0f4c37c256154cce2
A-Keyz <akeisling@students.ccu.edu>
2024-04-30 23:02:45 -0400

    fonts and images

--- tide-cast/src/app/dashboard/page.tsx
+++ tide-cast/src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
+import { lusitana } from '@/app/dashboard/fonts';
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <>
-      <div>Hello World!</div>
+      <div>
+        <Image 
+          src={"/desktop-hero.jpeg"}
+          width={1024}
+          height={760}
+          className='hidden md:block'
+          alt='Desktop hero image'
+        />
+        <Image 
+          src={"/mobile-hero.jpeg"}
+          width={560}
+          height={620}
+          className='block md:hidden'
+          alt='mobile hero image'
+        />
+      </div>
     </>
   )
 }
